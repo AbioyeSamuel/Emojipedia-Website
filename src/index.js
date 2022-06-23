e652160002255c3dcb9ab89c6203057fcc689f84
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-23 16:35:48 +0100

    Arrow Function

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+//Arrow function Practice.
+var numbers = [3, 56, 2, 48, 5];
+function Square(x) {
+  return x * x;
+}
+const newNumbers = numbers.map(Square);
+console.log(newNumbers);
