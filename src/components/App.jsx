0f82a17feec688d3e8d7bdf05cbe9a3365210c8b
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-08-18 10:15:25 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
+function eachEmoji(emojipedias){
+}
 function App() {
   return (
     <div>
@@ -10,14 +12,7 @@ function App() {
       </h1>
       <div>
         <dl className="dictionary">
-          {emojipedia.map((emojipedias) => (
-            <Entry
-              key={emojipedias.id}
-              emoj={emojipedias.emoji}
-              name={emojipedias.name}
-              mean={emojipedias.meaning}
-            />
-          ))}
+          {emojipedia.map(eachEmoji)}
           {/* <Entry emoj={props.emoji} name={props.name} mean={props.meaning} /> */}
         </dl>
       </div>
